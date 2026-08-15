d4edba05fe7c05b633b2655f34851fc5fd171a05
byronsharman <byron.n.sharman@gmail.com>
2026-08-15 12:37:23 -0600

    remove steam

--- configuration.nix
+++ configuration.nix
@@ -155,7 +155,6 @@
   programs.fish.enable = true;
   programs.niri.enable = true;
   programs.nix-ld.enable = true;
-  programs.steam.enable = true;
 
   xdg.mime.defaultApplications = {
     "application/pdf" = "org.gnome.Papers.desktop";
